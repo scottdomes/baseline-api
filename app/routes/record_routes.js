c8be890b49e55d625c3a1c08e08c78883151d468
YScott Domes <scott@musefind.com>
2017-10-16 19:33:58 -0700

    Got record finding working

--- app/routes/record_routes.js
+++ app/routes/record_routes.js
@@ -1,14 +1,18 @@
 var RecordModel = require('../models/record');
 
 module.exports = function(app, db) {
+  app.get('/records/:id', (req, res) => {
+    RecordModel.findById(req.params.id, function(err, model) {
+      res.send(model);
+    });
+  });
+
   app.post('/records', (req, res) => {
-    // You'll create your note here.
     var entry = new RecordModel({ type: req.body.type, value: req.body.value });
 
     entry.save(function(error) {
-      //This saves the information you see within that Bee declaration (lines 4-6).
       console.log('Your entry has been saved.');
-      res.send(entry)
+      res.send(entry);
       if (error) {
         console.error(error);
       }
