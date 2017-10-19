572bd782067d97bbd39eb2b428c6e1d27018ca4e
YScott Domes <scott@musefind.com>
2017-10-18 19:01:28 -0700

    Added database env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
